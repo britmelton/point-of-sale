e8ff18b365bf8e2b4513180a188e72dc5e693873
britmelton <brittany134340@gmail.com>
2023-04-25 12:38:49 -0400

    unit tests for adding products to order

---
+++ domain-spec/WhenAddingProductToOrder.cs
@@ -0,0 +1,32 @@
+﻿using domain;
+using FluentAssertions;
+
+namespace domain_spec
+{
+    public class WhenAddingProductToOrder
+    {
+        [Fact]
+        public void ThenProductExistsInOrder()
+        {
+            var order = new Order();
+            var product = new Product("product1", 3.99m, "abc345");
+            order.Products.Add(product);
+
+            order.Products.Should().Contain(product);
+        }
+
+        [Fact]
+        public void WithMultipleProduct_ThenAllProductsExistInOrder()
+        {
+            var order = new Order();
+            var product = new Product("product1", 3.99m, "abc345");
+            var product2 = new Product("product2", 4.99m, "abc789");
+
+            order.Products.Add(product);
+            order.Products.Add(product2);
+
+            order.Products.Should().Contain(product);
+            order.Products.Should().Contain(product2);
+        }
+    }
+}
--- domain/Order.cs
+++ domain/Order.cs
@@ -6,11 +6,18 @@ namespace domain
     {
         public Guid Id { get; }
         public string OrderNumber { get; }
+        public List<Product> Products { get; set; }
 
         public Order(string? orderNumber = default, Guid id = default)
         {
+            Products = new();
             OrderNumber = orderNumber == default ? GenerateOrderNumber() : orderNumber;
             Id = id == default ? Guid.NewGuid() : id;
+        }
+
+        public void Add(Product product)
+        {
+            Products.Add(product);
         }
 
         private static string GenerateOrderNumber()
