e6bda8aa69dc3174092c32027ebed85a9dbfc747
britmelton <brittany134340@gmail.com>
2023-04-25 11:00:57 -0400

    unit test for setting Order Id

---
+++ domain-spec/WhenStartingANewOrder.cs
@@ -0,0 +1,16 @@
+﻿using domain;
+using FluentAssertions;
+
+namespace domain_spec
+{
+    public class WhenStartingANewOrder
+    {
+        [Fact]
+        public void ThenOrderNumberIsRegistered()
+        {
+            var order = new Order();
+
+            order.Id.Should().NotBeEmpty();
+        }
+    }
+}
---
+++ domain/Order.cs
@@ -0,0 +1,12 @@
+﻿namespace domain
+{
+    public class Order
+    {
+        public Guid Id { get; }
+
+        public Order(Guid id = default)
+        {
+            Id = id == default ? Guid.NewGuid() : id;
+        }
+    }
+}
